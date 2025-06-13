49a3d31027d4c28fb0a769990a14a1313df39e50
Shrutilap <150513066+Shrutilap@users.noreply.github.com>
2025-06-13 15:06:26 +0530

    Update user.js

--- inc_backend/BloodNova/models/user.js
+++ inc_backend/BloodNova/models/user.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/blood_nova', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// })
-// .then(() => console.log("MongoDB Connected"))
-// .catch(err => console.error("MongoDB Connection Error:", err));
+
 const passportLocalMongoose=require("passport-local-mongoose");
 
 const userSchema = new mongoose.Schema({
@@ -12,7 +7,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    username: { // 🟢 Username field for login
+    username: { 
         type: String,
         required: true,
         unique: true
@@ -35,17 +30,5 @@ const userSchema = new mongoose.Schema({
 
 userSchema.plugin(passportLocalMongoose);
 const User = mongoose.model('User', userSchema);
-
-// const newUser = new User({
-//     name: "John Doe",
-//     email: "johndoe@example.com",
-//     password:"abc"
-// });
-
-// newUser.save()
-//     .then(() => console.log("User saved successfully"))
-//     .catch((err) => console.log("Error saving user:", err));
-
-
 
 module.exports = User;
